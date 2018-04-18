3f16e13bb2b3310d81fedb1e209fd8ba4a827f34
rnAx <bhy5259771@163.com>
2018-04-18 23:03:12 +0800

    completed search function and screening style

--- js/index.js
+++ js/index.js
@@ -1,11 +1,28 @@
-$(function() {
+$(function () {
     // 请求影片信息
-    $.ajax({
-        url: "https://api.douban.com/v2/movie/subject/1764796",
-        type: "get",
-        dataType: "jsonp",
-        success: function (data) {
-            console.log(data);
+    // $.ajax({
+    //     url: "https://api.douban.com/v2/movie/in_theaters?city=上海",
+    //     type: "get",
+    //     dataType: "jsonp",
+    //     success: function (data) {
+    //         console.log(data);
+    //     }
+    // });
+
+    /* 电影搜索 */
+    $("#searchBtn").click(function () {
+        let keyword = $("#searchInput").val();
+        if (keyword) {
+            $.ajax({
+                url: "https://api.douban.com/v2/movie/search?q=" + keyword,
+                type: "get",
+                dataType: "jsonp",
+                success: function (data) {
+                    console.log(data);
+                }
+            });
+        } else {
+            alert("请输入内容!");
         }
     });
 });
