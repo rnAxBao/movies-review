e15fca7f956c30d4f273cb62f5ade4d675644f9a
rnAx <bhy5259771@163.com>
2018-04-19 23:08:26 +0800

    completed slider function(no loop)

--- js/index.js
+++ js/index.js
@@ -9,31 +9,40 @@ $(function () {
         },
         dataType: "jsonp",
         success: function (data) {
-            console.log(data);
             let res = data;
+            console.log(res);
             let ulWrap = $(".screening .ui-slide-content");
-            for (let i = 0; i < res.total; i++) {
+            for (let i = 0; i < res.subjects.length; i++) {
                 let li = $('<li class="ui-slide-item"></li>');
-                if (i < res.subjects.length) {
-                    addMovieItem(ulWrap, li, res, i);
-                } else {
-                    // $.ajax({
-                    //     url: "https://api.douban.com/v2/movie/in_theaters",
-                    //     type: "get",
-                    //     data: {
-                    //         city: "上海",
-                    //         start: 20
-                    //     },
-                    //     dataType: "jsonp",
-                    //     success: function (data) {
-                    //         let res2 = data;
-                    //         console.log(i);
-                    //         let j = i - 20;
-                    //         addMovieItem(ulWrap, li, res2, j);
-                    //     }
-                    // });
-                }
+                addMovieItem(ulWrap, li, res, i);
             }
+
+            $.ajax({
+                url: "https://api.douban.com/v2/movie/in_theaters",
+                type: "get",
+                data: {
+                    city: "上海",
+                    start: 20
+                },
+                dataType: "jsonp",
+                success: function (data) {
+                    let res2 = data;
+                    for (let j = 0; j < res2.subjects.length; j++) {
+                        let li = $('<li class="ui-slide-item"></li>');
+                        addMovieItem(ulWrap, li, res2, j);
+                    }
+
+                    /* 给图片绑定懒加载 */
+                    $("img.lazy").lazyload({
+                        placeholder: "img/grey.gif",
+                        effect: "fadeIn",
+                        threshold: 3000
+                    });
+                }
+            });
+
+            /* 取得slider的总页数 */
+            $(".show-page .ui-slide-max").text(Math.ceil(res.total / 5));
 
             function addMovieItem(faEle, sonEle, data, index) {
                 let movieId = data.subjects[index].id;
@@ -44,7 +53,7 @@ $(function () {
                             <ul>
 								<li class="poster">
 									<a href="">
-									    <img src=${moviePic} alt=${movieName}>
+									    <img class="lazy" data-original=${moviePic} alt=${movieName} width="128" height="179">
 								    </a>
 								</li>
 								<li class="title">
@@ -73,41 +82,34 @@ $(function () {
                     sonEle.find(".rating").empty().append('<span class="text-tip">暂无评分</span>');
                 }
             }
-            // let lastLiCount = data.subjects.length % 5;
-            // for (let i = 0; i < 5; i++) {
-            //     if (i < lastLiCount) {
-            //         let html = `<li class="ui-slide-item" data-id="${movieId}">
-            // 				<ul>
-            // 					<li class="poster">
-            // 						<a href="">
-            // 							< img src = "${moviePic}"
-            // 							alt = "${movieName}" >
-            // 						</a>
-            // 					</li>
-            // 					<li class="title">
-            // 						<a href="">${movieName}</a>
-            // 					</li>
-            // 					<li class="rating">
-            // 						<span class="rating-star"></span>
-            // 						<span class="subject-rate">${movieRating}</span>
-            // 					</li>
-            // 					<li class="ticket_btn">
-            // 						<span>
-            // 							<a href="" target="_blank">加入影库</a>
-            // 						</span>
-            // 					</li>
-            // 				</ul>
-            //             </li>`;
-            //         let movieId = res.subjects[i].id;
-            //         let moviePic = res.subjects[i].images.small;
-            //         let movieName = res.subjects[i].title;
-            //         let movieRating = res.subjects[i].rating.average;
-
-            //     } 
-            // }
         }
     });
 
+    /* 绑定slider事件 */
+    let slideIndex = 1;
+    let slideX = 0;
+    $(".paging .btn-next").click(function () {
+        slideIndex++;
+        slideX += -700;
+        if (slideIndex == 8) {
+            slideIndex = 1;
+            slideX = 0;
+        }
+        /* 显示当前页数 */
+        $(".show-page .ui-slide-index").text(slideIndex);
+        $(".screening .ui-slide-content").css("transform", "translateX(" + slideX + "px)");
+    });
+    $(".paging .btn-prev").click(function () {
+        slideIndex--;
+        slideX += 700;
+        if (slideIndex == 0) {
+            slideIndex = 7;
+            slideX = -4200;
+        }
+        /* 显示当前页数 */
+        $(".show-page .ui-slide-index").text(slideIndex);
+        $(".screening .ui-slide-content").css("transform", "translateX(" + slideX + "px)");
+    });
     /* 电影搜索 */
     $("#searchBtn").click(function () {
         let keyword = $("#searchInput").val();
